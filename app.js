80dace23d299a694b3491509d34797fcce5c14ab
Daniel Carter <dtcarter84@gmail.com>
2017-11-12 15:52:42 -0500

    added enterDoor function
    Added enterDoor function

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ var speakeasy = prompt("Psst...What's the password?");
 
 hoochButton.addEventListener('click', () => {
     prompt("Psst...What's the password?");
-};
+       function enterDoor () {
+        if (speakeasy.toUpperCase() === 'BOURBON') {
+            document.write("<p>That's it! You may enter!</p>");
+            } else if {
+                document.write("<p>That ain't it. Try again. It's a liquor made in Kentucky.</p>");
+            } else if {
+                document.write("<p>Nope. Try again. Say...are you a fan of BOURBON?</p>");
+            } else {
+                document.write("<p>I'm a nice guy. Normally I don't do this, but you can come in.</p>");
+            }
+        }
+    });
     
     
     
