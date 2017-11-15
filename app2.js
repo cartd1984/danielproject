7848e4b31329f7869bd4812fcfa34982a5c438a4
Daniel Carter <dtcarter84@gmail.com>
2017-11-14 23:39:14 -0500

    changes to fix cancel button issue
    Cancel button would click to next page before; now it will force you to click on image and answer questions

--- app2.js
+++ app2.js
@@ -1,50 +1,56 @@
 const hoochButton = document.getElementById("hoochButton");
 const fancyButton = document.getElementById("fancyButton");
 
+//Code for Door 1 - to speakeasy1920.html
+
 hoochButton.addEventListener('click', () => {
     var passwordGuess = prompt("Psst...What's the password?");
-    var password = false;
     var speakeasy = "BOURBON";
-    if (passwordGuess.toUpperCase() == speakeasy) {
+    if (!passwordGuess) {
+        alert("Nice try, Jack. You gotta at least try! Click on the door again and guess the password.");
+        } else if (passwordGuess.toUpperCase() === speakeasy) {
             alert("That's it! You may enter!");
-            var password = true;
-            } else if (passwordGuess.toUpperCase() != speakeasy) {
+            window.location.href="speakeasy1920.html";
+            } else if (passwordGuess.toUpperCase() !== speakeasy) {
                 var passwordGuess2 = prompt("That ain't it. Try again. It's a liquor made in Kentucky.");
-            } if (passwordGuess2.toUpperCase() == speakeasy) {
+            } if (passwordGuess2.toUpperCase() === speakeasy) {
                 alert("Congrats! You may enter.");
-                var password = true;
-            } else if (passwordGuess2.toUpperCase() != speakeasy) {
+                window.location.href="speakeasy1920.html";
+            } else if (passwordGuess2.toUpperCase() !== speakeasy) {
                 var passwordguess3 = prompt("Nope. Try again. Say...are you a fan of BOURBON?");
-                if (passwordguess3.toUpperCase() == speakeasy) {
+                if (passwordguess3.toUpperCase() === speakeasy) {
                         alert("Dude, finally! You may now enter.");
-                        var password = true;
+                        window.location.href="speakeasy1920.html";
             } else {
                 alert("I'm a nice guy. I'll got ahead and let you in.");
-                var password = true;
+                window.location.href="speakeasy1920.html";
             }
         }
 });
 
+//Code for Door 2 - to speakeasy2010.html
+
 fancyButton.addEventListener('click', () => {
     var passwordGuess10 = prompt("You know the drill...What's the password?");
-    var password2 = false;
     var speakeasy2 = "VODKA";
-    if (passwordGuess10.toUpperCase() == speakeasy2) {
+    if (!passwordGuess10) {
+        alert("Haha...that's cute. Trying to get around guessing the password like that. Click on the door and try again.");
+    } else if (passwordGuess10.toUpperCase() == speakeasy2) {
             alert("You may enter.");
-            var password2 = true;
+            window.location.href="speakeasy2010.html";
             } else if (passwordGuess10.toUpperCase() != speakeasy2) {
                 var passwordGuess20 = prompt("Sorry, man. Say...ever been to Russia?");
             } if (passwordGuess20.toUpperCase() == speakeasy2) {
                 alert("Good work! Enjoy!");
-                var password = true;
+                window.location.href="speakeasy2010.html";
             } else if (passwordGuess20.toUpperCase() != speakeasy2) {
                 var passwordguess30 = prompt("Nope. Try again. You like VODKA in your potatoes?");
                 if (passwordguess30.toUpperCase() == speakeasy2) {
                         alert("My dog could have gotten in at this point! You may now enter.");
-                        var password = true;
+                        window.location.href="speakeasy2010.html";
             } else {
                 alert("I may get kicked out, but the hey, I'll let you in anyway.");
-                var password = true;
+                window.location.href="speakeasy2010.html";
             }
         }
 });
